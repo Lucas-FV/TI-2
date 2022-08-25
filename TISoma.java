ae67838725da78588d2df27d28198d46043a0411
Lucas-FV <104392733+Lucas-FV@users.noreply.github.com>
2022-08-25 17:15:28 -0300

    Add files via upload

---
+++ TISoma.java
@@ -0,0 +1,15 @@
+class TISoma{
+
+    public static int soma(int x, int y){
+        return x + y;
+    }
+
+    public static void main(String[] args) {
+        
+        int x = 6, y = 100;
+
+        System.out.print(soma(x,y));
+
+
+    }
+}
